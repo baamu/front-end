e5221ab8e3fd764771d21c6e64c34facd9f83b50
Oshan96 <oshanmen@gmail.com>
2019-11-13 12:28:37 +0530

    signup fixed

--- src/app/userSignIn/sign-in.component.ts
+++ src/app/userSignIn/sign-in.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { map } from "rxjs/operators";
 
 @Component({
   selector: 'app-sign-in',
@@ -7,9 +10,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(public fb: FormBuilder, private http: HttpClient) { }
+
+  formSubmit(){
+  
+    var user = {
+      email : this.form.get('email').value,
+      username : this.form.get('username').value,
+      name : this.form.get('name').value,
+      dob : this.form.get('dob').value,
+      nic : this.form.get('nic').value,
+      password : this.form.get('password').value
+    }
+
+    const hdr = new HttpHeaders().set('Content-Type', 'application/json')
+      
+    console.log(user)
+
+    this.http.post<HttpResponse<any>>('http://localhost:8080/api/public/user/register', user, {headers: hdr, observe : "response"})
+    .pipe(
+      map(response => {
+        // console.log(response.headers.get("authorization"))
+        return response.body
+      })
+    )
+    .subscribe(response => {
+      console.log(response)
+    },
+
+    error => {
+      console.log("error ", error)
+    }
+    
+    )
+
+  }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      email: [''],
+      username: [''],
+      name: [''],
+      dob: [''],
+      nic: [''],
+      password: [''],
+      repass:['']
+    })
   }
 
 }
