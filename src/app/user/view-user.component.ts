7f284496f27b5c8c8c28943d45da658bc0606ecc
Oshan96 <oshanmen@gmail.com>
2019-11-21 14:45:51 +0530

    - Fixed table pagnition in view downloads - Added table data filtering for view downloads - Added css

--- src/app/user/view-user.component.ts
+++ src/app/user/view-user.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import { AppService } from '../app.service';
-import { MatTableDataSource } from '@angular/material';
+import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
 
 export interface Download {
   id: string;
@@ -16,16 +16,31 @@ export interface Download {
   styleUrls: ['./view-user.component.css']
 })
 export class ViewUserComponent implements OnInit{
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  
   ngOnInit(): void {
 
     this.service.getOnGoingDownloads().subscribe(response => {
-      this.dataSource = new MatTableDataSource(response);
+      this.dataSource.data=response;
     })
+
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
  
   }
 
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
   displayedColumns: string[] = ['url', 'downloadedSize', 'fileSize', 'completed'];
-  dataSource : MatTableDataSource<Download[]>;
+  dataSource : MatTableDataSource<Download[]> = new MatTableDataSource();
 
   constructor(private service : AppService) {
     
